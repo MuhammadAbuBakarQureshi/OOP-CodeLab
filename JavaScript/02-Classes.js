e933d2b8e93637a41ba32de5587c130c184ef494
MuhammadAbuBakarQureshi <152699100+MuhammadAbuBakarQureshi@users.noreply.github.com>
2025-02-04 16:35:27 +0500

    Create 02-Classes.js

---
+++ JavaScript/02-Classes.js
@@ -0,0 +1,13 @@
+class car{
+    
+    constructor(model, numberPlate){
+
+        this.model = model;
+        this.numberPlate = numberPlate;
+    }
+}
+
+
+let cultus = new car(2012, "vw 308");
+
+console.log(cultus);
